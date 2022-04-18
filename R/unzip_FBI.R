575ec217fae859c15b56fd079a92c8e2a18dbf26
Gorka Navarrete <gorkang@gmail.com>
2022-04-18 19:30:52 +0100

    catch (2) issues

--- R/unzip_FBI.R
+++ R/unzip_FBI.R
@@ -3,6 +3,8 @@ unzip_FBI <- function(zip_folder = "data_raw/FBI/", output_folder = "data/FBI/",
   # List zips
   zips = list.files(zip_folder, recursive = TRUE, pattern = "zip", full.names = TRUE)
   
+  if (any(grepl("\\(2\\)", zips))) cli::cli_abort("(2) files in the data_raw/")
+  
   # Unzip in new folders
   zips_folders <- paste0(output_folder, unlist(regmatches(zips, gregexpr("[[:digit:]]+", zips))))
   1:length(zips_folders) %>% walk(~ dir.create(zips_folders[.x], recursive = TRUE))
