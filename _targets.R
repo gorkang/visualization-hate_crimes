fa40251253053bc83000bc4c928ec55746587216
G <gorkang@gmail.com>
2022-04-19 18:18:31 +0100

    update

--- _targets.R
+++ _targets.R
@@ -42,8 +42,11 @@ targets <- list(
   tar_target(SETUP, setup_project()),
   
   # GENERAL DATA PREPARATION
-  tar_target(UNZIP_FBI, unzip_FBI(zip_folder = "data_raw/FBI/", output_folder = "data/FBI/", SETUP = SETUP)),
+  tar_target(UNZIP_FBI, unzip_raw_files(zip_folder = "data_raw/FBI/", output_folder = "data/FBI/", SETUP = SETUP)),
+  tar_target(UNZIP_NCVS, unzip_raw_files(zip_folder = "data_raw/NCVS/", output_folder = "data/NCVS/", SETUP = SETUP)),
   
+  
+  ## FBI READ TABLES --------------------------------------------------------
   
   # Diccionaries
   tar_target(DICC_FBI_t1, prepare_DICC_FBI_t1()),
@@ -57,10 +60,17 @@ targets <- list(
   tar_target(DF_FBI_t1, prepare_t1(DF = DF_FBI_t1_raw, diccionary = DICC_FBI_t1)),
   tar_target(DF_FBI_t2, prepare_t2(DF = DF_FBI_t2_raw, diccionary = DICC_FBI_t2)),
   
-  tar_target(DF_FBI_t2_surface_ALL, prepare_data_FBI_t2_surface(DF = DF_FBI_t2, supra_sub = "sub", filter_offense_supra = "*")),
-  tar_target(PLOTS_FBI_t2_surface_ALL, plot_FBI_t1_surface(DF = DF_FBI_t2_surface_ALL, output_suffix = "ALL", table = 2)),
+  
+  ## NCVS READ TABLES -------------------------------------------------------
+  tar_target(DF_NCVS_t01_raw, prepare_data_NCVS(file_name = "hcv0519t01.csv", skip_n = 12, UNZIPPED = UNZIP_NCVS)),
   
   
+
+  # PLOTS ---------------------------------------------------------------
+
+
+  ## FBI ---------------------------------------------------------------------
+
   
   # Surface plots Data preparation
     # DF_FBI_t1 %>% distinct(bias_supra)
@@ -72,20 +82,31 @@ targets <- list(
   tar_target(DF_FBI_t1_surface_GenderIdentity, prepare_data_FBI_t1_surface(DF = DF_FBI_t1, supra_sub = "sub", filter_bias_supra = "Gender Identity:")),
   tar_target(DF_FBI_t1_surface_race, prepare_data_FBI_t1_surface(DF = DF_FBI_t1, supra_sub = "sub", filter_bias_supra = "Race/Ethnicity/Ancestry:")),
   
+  tar_target(DF_FBI_t2_surface_ALL, prepare_data_FBI_t2_surface(DF = DF_FBI_t2, supra_sub = "sub", filter_offense_supra = "*")),
+  
+  
   # Surface plots
+  # table 1
   tar_target(PLOTS_FBI_t1_surface_ALL, plot_FBI_t1_surface(DF = DF_FBI_t1_surface_ALL, output_suffix = "ALL", table = 1)),
   tar_target(PLOTS_FBI_t1_surface_ALL_supra, plot_FBI_t1_surface(DF = DF_FBI_t1_surface_ALL_supra, output_suffix = "ALL_supra", table = 1)),
-  # 
   tar_target(PLOTS_FBI_t1_surface_sexual, plot_FBI_t1_surface(DF = DF_FBI_t1_surface_sexual, output_suffix = "SexualOrientation", table = 1)),
   tar_target(PLOTS_FBI_t1_surface_religion, plot_FBI_t1_surface(DF = DF_FBI_t1_surface_religion, output_suffix = "Religion", table = 1)),
   tar_target(PLOTS_FBI_t1_surface_gender, plot_FBI_t1_surface(DF = DF_FBI_t1_surface_gender, output_suffix = "Gender", table = 1)),
   tar_target(PLOTS_FBI_t1_surface_GenderIdentity, plot_FBI_t1_surface(DF = DF_FBI_t1_surface_GenderIdentity, output_suffix = "GenderIdentity", table = 1)),
   tar_target(PLOTS_FBI_t1_surface_race, plot_FBI_t1_surface(DF = DF_FBI_t1_surface_race, output_suffix = "Race", table = 1)),
   
+  # table 2
+  tar_target(PLOTS_FBI_t2_surface_ALL, plot_FBI_t1_surface(DF = DF_FBI_t2_surface_ALL, output_suffix = "ALL", table = 2)),
   
-  # PLOTS
+  # General PLOTS
   tar_target(PLOTS_FBI_t1_lines, plot_FBI_t1_lines(DF = DF_FBI_t1)),
-  tar_target(PLOTS_FBI_t1_density, plot_FBI_t1_density(DF = DF_FBI_t1))
+  tar_target(PLOTS_FBI_t1_density, plot_FBI_t1_density(DF = DF_FBI_t1)),
+  
+  
+
+  # NCVS --------------------------------------------------------------------
+  
+  tar_target(PLOTS_NCVS, plot_NCVS(DF = DF_NCVS_t01_raw))
   
 )
 
