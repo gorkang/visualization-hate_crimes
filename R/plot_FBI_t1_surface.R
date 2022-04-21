e4f06420c76617be8568aa5e8ee64e2a2f78e769
G <gorkang@gmail.com>
2022-04-21 12:15:22 +0100

    add parameters to control colorscale, absolute_relative, width and height, saving outputs... also, aspect ratio depends on number of vars plotted

--- R/plot_FBI_t1_surface.R
+++ R/plot_FBI_t1_surface.R
@@ -1,15 +1,21 @@
-plot_FBI_t1_surface <- function(DF, output_suffix, table) {
+plot_FBI_t1_surface <- function(DF, output_suffix, table, save_outputs = TRUE, height = NULL, width = NULL, colorscale = "Viridis", absolute_relative = "absolute") {
   
   # DEBUG
   # DF = DF_matrix
+  
+  # targets::tar_load(DF_FBI_t1_surface_ALL)
   # DF = DF_FBI_t1_surface_ALL
   # output_suffix = "ALL"
   # table = 1
+  # colorscale = "Viridis"
+  # height = NULL
+  # width = NULL
+  # absolute_relative = "absolute"
 
+  # DF = DF_FBI_t1_surface_ALL
   # DF = DF_FBI_t1_surface_gender
-  # output_suffix = "ALL"
+  # output_suffix = "Race"
   # table = 1
-  
   
   # PLOT --------------------------------------------------------------------
   
@@ -35,6 +41,7 @@ plot_FBI_t1_surface <- function(DF, output_suffix, table) {
   #              )) %>% 
   #   pull(bias_motivation_short)
   
+  name_z = ifelse(absolute_relative == "absolute", "Incidents", "Incidents/100,000")
   
   spike_color = "#111111"
   highlight_color = "#41a7b3"
@@ -50,23 +57,30 @@ plot_FBI_t1_surface <- function(DF, output_suffix, table) {
   n_ticks_x = length(x_axis)
   n_ticks_y = length(y_axis_labels)
   
-  PLOT = plotly::plot_ly(
+  
+  # Aspect ratio of plot depends on nunmber of lables in y axis
+  aspect_ratio_y = ifelse(length(y_axis) > 2, 2, 1)
+  
+
+  # plot_ly --------------------------------------------------------------------
+
+  PLOT = plotly::plot_ly(height = height, width = width,
     x = ~ x_axis,
     y = ~ y_axis,
     z = ~ z_axis,
     type = "surface",
     # colorbar = list(title = "Incidents", ticks = "outside"),
-    colorscale = "Viridis", # Blackbody,Bluered,Blues,Cividis,Earth,Electric,Greens,Greys,Hot,Jet,Picnic,Portland,Rainbow,RdBu,Reds,Viridis,YlGnBu,YlOrRd.
+    colorscale = colorscale, # Blackbody,Bluered,Blues,Cividis,Earth,Electric,Greens,Greys,Hot,Jet,Picnic,Portland,Rainbow,RdBu,Reds,Viridis,YlGnBu,YlOrRd.
     showlegend = FALSE,
     showscale = FALSE, # legend dissapears
     connectgaps = FALSE, # FILLS NA with 0 # Freezes when overing over NA value
     opacity = 1, # Transparency of surface 0-1
     
     # Template for hover
-    hovertemplate = paste(
+    hovertemplate = paste0(
       "<b>%{y}</b><br>",
       "Year: %{x:%Y}<br>",
-      "Incidents: %{z}",
+      name_z, " : %{z}",
       "<extra></extra>"
     ),
     # hoverlabel = list(bgcolor = "rgba(255,255,255,0.75)"),
@@ -109,7 +123,7 @@ plot_FBI_t1_surface <- function(DF, output_suffix, table) {
                      ),
         
         # INCIDENTS
-        zaxis = list(title = "incidents",
+        zaxis = list(title = name_z,
                      showspikes = show_spikes,
                      spikecolor = spike_color,
                      separatethousands = TRUE,
@@ -118,15 +132,19 @@ plot_FBI_t1_surface <- function(DF, output_suffix, table) {
                      col = 1), 
         
         # Aspect ratio of plot
-        aspectratio = list(x = 1, y = 2, z = 1),
+        aspectratio = list(x = 1, y = aspect_ratio_y, z = 1),
         
         # Initial position of camera
         # https://plotly.com/r/reference/#layout-scene-camera
+        # x: years y: bias z: incidents
         camera = list(
           eye = list(x = 2, y = 1, z = 1),
-          # eye = list(x = 1, y = 1, z = 0),
           center = list(x = 0.5, y = 0.2, z = 0),
-          up = list(x = 0, y = 0, z = 1))
+          up = list(x = 0, y = 0, z = 1)
+          # eye = list(x = 0.5, y = 3, z = 1),
+          # center = list(x = 0, y = 0, z = -0.5), # - up +down
+          # up = list(x = 0, y = 0, z = 1) # which up 0/1
+          )
       )
     ) %>% 
     
@@ -134,7 +152,7 @@ plot_FBI_t1_surface <- function(DF, output_suffix, table) {
     # https://plotly.com/r/reference/#surface-hidesurface
     # show: show 3d mesh
     # highlight: ADD highlight lines over surface
-    add_surface(name = " ",
+    plotly::add_surface(name = " ",
       contours = list(
         x = list(show = TRUE,
                  width = 1,
@@ -157,15 +175,17 @@ plot_FBI_t1_surface <- function(DF, output_suffix, table) {
   
   
   # Export images --------------------------------------
+
+  if (save_outputs == TRUE) {
   
+    # Save png
+      # Need to install kaleido (python): https://search.r-project.org/CRAN/refmans/plotly/html/save_image.html
+    plotly::save_image(PLOT, paste0("outputs/PLOTs/surface/plot_FBI_t", table, "_surface_", output_suffix, ".png"), width = 1600, height = 900)
   
-  # Save png
-    # Need to install kaleido (python): https://search.r-project.org/CRAN/refmans/plotly/html/save_image.html
-  plotly::save_image(PLOT, paste0("outputs/PLOTs/surface/plot_FBI_t", table, "_surface_", output_suffix, ".png"), width = 1600, height = 900)
-  
-  # Save interactive html
-  htmlwidgets::saveWidget(PLOT, paste0("outputs/PLOTs/surface/plot_FBI_t", table, "_surface_", output_suffix, ".html"), selfcontained = TRUE, libdir = "lib")
+    # Save interactive html
+    htmlwidgets::saveWidget(PLOT, paste0("outputs/PLOTs/surface/plot_FBI_t", table, "_surface_", output_suffix, ".html"), selfcontained = TRUE, libdir = "lib")
+  }
   
   return(PLOT)
-  
+
 }
